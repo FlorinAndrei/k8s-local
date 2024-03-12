dfed0c14a637221c6ce4d895611ef80d09ff4f44
FlorinAndrei <901867+FlorinAndrei@users.noreply.github.com>
2024-03-12 12:18:09 -0700

    use kubectl 1.28.3

--- 02-download-utils.sh
+++ 02-download-utils.sh
@@ -6,7 +6,7 @@ chmod 755 cfssl
 wget https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssljson_1.6.4_linux_amd64
 mv cfssljson_1.6.4_linux_amd64 cfssljson
 chmod 755 cfssljson
-wget https://storage.googleapis.com/kubernetes-release/release/v1.29.0/bin/linux/amd64/kubectl
+wget https://storage.googleapis.com/kubernetes-release/release/v1.28.3/bin/linux/amd64/kubectl
 chmod 755 kubectl
 
 echo "Move cfssl, cfssljson, and kubectl to a directory in the PATH."
