6091f1948493af098c9418e8d37cf5b5c0a26d85
FlorinAndrei <901867+FlorinAndrei@users.noreply.github.com>
2024-03-13 16:31:12 -0700

    create pod network routes

---
+++ 11-pod-network-routes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+. ./variables.sh
+
+sudo ip route add ${cluster_cidr_worker[worker-0]} via ${node_ip[worker-0]}
+sudo ip route add ${cluster_cidr_worker[worker-1]} via ${node_ip[worker-1]}
+sudo ip route add ${cluster_cidr_worker[worker-2]} via ${node_ip[worker-2]}
+
+echo
+sudo ip route
