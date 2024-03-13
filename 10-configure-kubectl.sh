f96618d5b8bccb0b76d26ef78bf037b756afcf96
FlorinAndrei <901867+FlorinAndrei@users.noreply.github.com>
2024-03-13 15:58:53 -0700

    configure kubectl

---
+++ 10-configure-kubectl.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+. ./variables.sh
+
+KUBERNETES_PUBLIC_ADDRESS="$main_host_ip"
+
+kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca/ca.pem \
+    --embed-certs=true \
+    --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443
+
+kubectl config set-credentials admin \
+    --client-certificate=ca/admin.pem \
+    --client-key=ca/admin-key.pem
+
+kubectl config set-context kubernetes-the-hard-way \
+    --cluster=kubernetes-the-hard-way \
+    --user=admin
+
+kubectl config use-context kubernetes-the-hard-way
+
+echo
+kubectl version
+echo
+kubectl get nodes -o wide
