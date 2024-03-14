45b6f59d91251050e41223fbb633822ed18f2707
FlorinAndrei <901867+FlorinAndrei@users.noreply.github.com>
2024-03-13 17:49:20 -0700

    better live messages

--- 11-pod-network-routes.sh
+++ 11-pod-network-routes.sh
@@ -2,9 +2,13 @@
 
 . ./variables.sh
 
+echo "Setting pod network routes..."
 sudo ip route add ${cluster_cidr_worker[worker-0]} via ${node_ip[worker-0]}
 sudo ip route add ${cluster_cidr_worker[worker-1]} via ${node_ip[worker-1]}
 sudo ip route add ${cluster_cidr_worker[worker-2]} via ${node_ip[worker-2]}
 
 echo
+echo "Current routes:"
+echo
+
 sudo ip route
