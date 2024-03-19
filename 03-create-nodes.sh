72e02ba8a8111aa9f984111cad7d8a238ce13614
FlorinAndrei <901867+FlorinAndrei@users.noreply.github.com>
2024-03-18 18:39:26 -0700

    kubeadm conversion start

--- 03-create-nodes.sh
+++ 03-create-nodes.sh
@@ -30,6 +30,25 @@ users:
 runcmd:
   - echo "AllowUsers $username" >> /etc/ssh/sshd_config
   - systemctl restart sshd
+  - apt-get update
+  - apt-get install -y apt-transport-https ca-certificates curl gpg gnupg-agent software-properties-common wget
+  - curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+  - echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+  - apt-get update
+  - apt-get install -y kubelet kubeadm kubectl
+  - apt-mark hold kubelet kubeadm kubectl
+  - systemctl enable --now kubelet
+  - for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
+  - wget --progress=bar:force:noscroll https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/containerd.io_1.6.28-2_amd64.deb
+  - wget --progress=bar:force:noscroll https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-ce_25.0.4-1~ubuntu.22.04~jammy_amd64.deb
+  - wget --progress=bar:force:noscroll https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-ce-cli_25.0.4-1~ubuntu.22.04~jammy_amd64.deb
+  - wget --progress=bar:force:noscroll https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-buildx-plugin_0.13.0-1~ubuntu.22.04~jammy_amd64.deb
+  - wget --progress=bar:force:noscroll https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-compose-plugin_2.24.7-1~ubuntu.22.04~jammy_amd64.deb
+  - dpkg -i containerd.io_1.6.28-2_amd64.deb docker-ce_25.0.4-1~ubuntu.22.04~jammy_amd64.deb docker-ce-cli_25.0.4-1~ubuntu.22.04~jammy_amd64.deb docker-buildx-plugin_0.13.0-1~ubuntu.22.04~jammy_amd64.deb docker-compose-plugin_2.24.7-1~ubuntu.22.04~jammy_amd64.deb
+  - wget --progress=bar:force:noscroll https://github.com/containernetworking/plugins/releases/download/v1.4.1/cni-plugins-linux-amd64-v1.4.1.tgz
+  - mkdir -p /opt/cni/bin
+  - tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.4.1.tgz
+  - rm -f /*.deb /*.tgz
 EOF
 
 sudo cloud-localds -v /var/lib/libvirt/images/$h/$h-seed.qcow2 \
@@ -72,6 +91,33 @@ boot_status=`ssh $ssh_opts ubuntu@${node_ip[$h]} 'sudo systemctl is-system-runni
 cloud_init_status=`ssh $ssh_opts ubuntu@${node_ip[$h]} 'sudo cloud-init status'`
 sleep 1
 done
+done
+
+
+echo
+echo "Reconfigure containerd, load modules, and configure sysctl:"
+echo
+
+for instance in ${nodes}; do
+scp $ssh_opts config.toml $username@${node_ip[$instance]}:
+scp $ssh_opts k8s.conf-modules $username@${node_ip[$instance]}:
+scp $ssh_opts k8s.conf-sysctl $username@${node_ip[$instance]}:
+
+ssh $ssh_opts $username@${node_ip[$instance]} << ENDSSH
+sudo mv /home/$username/config.toml /etc/containerd/config.toml
+sudo chown root:root /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+sudo mv /home/$username/k8s.conf-modules /etc/modules-load.d/k8s.conf
+sudo chown root:root /etc/modules-load.d/k8s.conf
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo mv /home/$username/k8s.conf-sysctl /etc/sysctl.d/k8s.conf
+sudo chown root:root /etc/sysctl.d/k8s.conf
+sudo sysctl --system
+
+ENDSSH
 done
 
 echo
--- README.md
+++ README.md
@@ -9,3 +9,17 @@ https://medium.com/@art.vasilyev/use-ubuntu-cloud-image-with-kvm-1f28c19f82f8
 **INCOMPLETE - STILL UNDER CONSTRUCTION!!!**
 
 Come back later, I expect this repo to be complete in a few days (?).
+
+https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/
+
+https://kubernetes.io/docs/concepts/cluster-administration/addons/
+
+https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/configure-cgroup-driver/
+
+https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+
+https://github.com/containerd/containerd/blob/main/docs/getting-started.md
+
+https://docs.docker.com/engine/install/ubuntu/
+
+https://medium.com/@sven_50828/setting-up-a-high-availability-kubernetes-cluster-with-multiple-masters-31eec45701a2
